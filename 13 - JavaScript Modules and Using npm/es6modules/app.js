80393884f688a555b157cc6f5a4677b4dc6e4318
Stephanie Nemeth <traumverloren@gmail.com>
2016-12-31 18:41:40 +0100

    finish es6 lesson 46 on making modules

--- 13 - JavaScript Modules and Using npm/es6modules/app.js
+++ 13 - JavaScript Modules and Using npm/es6modules/app.js
@@ -1,8 +1,13 @@
 /*eslint-disable */
-
 import { uniq } from 'lodash';
 import jsonp from 'jsonp';
+import { apiKey, url, sayHi } from './src/config';
+
+console.log(apiKey, url);
 
 const ages = [1,1,4,52,12,4];
 
 console.log(uniq(ages));
+
+
+sayHi('wes');
---
+++ 13 - JavaScript Modules and Using npm/es6modules/src/config.js
@@ -0,0 +1,11 @@
+// named export
+export const apiKey = 'abc123';
+
+// export default apiKey;
+
+
+export const url = "https://stephanie.lol";
+
+export function sayHi(name) {
+  console.log(`Hello there ${name}`);
+}
